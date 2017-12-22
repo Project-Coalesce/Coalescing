58dffb2cd926dcc86e796a8ebd88053bd80d4e13
NJDaeger <noahdaeger@hotmail.com>
2017-12-22 17:35:45 -0500

    Refactors. - reformatted code style. - created an installer. - small command changes.

--- src/main/java/com/coalesce/coalescing/Updater.java
+++ src/main/java/com/coalesce/coalescing/Updater.java
@@ -1,20 +1,30 @@
 package com.coalesce.coalescing;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Stream;
 
-import static java.lang.System.*;
-
-public class Updater {
+public class Updater extends Thread {
 
     private final Map<String, String> fileNames = new HashMap<>();
 
     private final String pluginFolder, updatesFolder;
 
+    /*
+    
+    args[0] - the plugin folder
+    args[1] - updates folder
+    
+    args[+] - the files. format: oldFile:newFile
+    
+     */
+    
     private Updater(String pluginFolder, String updatesFolder, String[] files) {
         this.pluginFolder = pluginFolder;
         this.updatesFolder = updatesFolder;
@@ -22,25 +32,31 @@ public class Updater {
         Stream.of(files).forEach(
                 name -> {
                     String[] plugins = name.split(":");
-                    fileNames.put(plugins[0], plugins[1]);
+                    fileNames.put(plugins[0]/*old*/, plugins[1]/*new*/);
                 }
         );
-        update();
+        try {
+            update();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    private void update() {
+    private void update() throws Exception {
         File plugins = new File(pluginFolder);
         Stream.of(plugins.listFiles()).filter(file -> fileNames.keySet().contains(file.getName())).forEach(File::delete);
-
+        
         File updates = new File(updatesFolder);
         Stream.of(updates.listFiles()).forEach(file -> {
             try {
                 Files.move(Paths.get(file.getPath()), Paths.get(plugins.getPath(), file.getName()), StandardCopyOption.REPLACE_EXISTING);
-            } catch (Exception ex) {
-                ex.printStackTrace();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
             }
         });
-        exit(1);
+        
     }
 
     public static void main(String[] args) {
